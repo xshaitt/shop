0ab4938c90364a47646909c28f8d682774bca603
zouke <zouke1220@126.com>
2019-05-07 20:00:46 +0800

    修改连接地址

--- resources/views/doc/main.blade.php
+++ resources/views/doc/main.blade.php
@@ -62,11 +62,11 @@
       </button>
       <div>
         <a href="#" class="navbar-brand">上海黑之白</a>
-        <a href="/doc/documents" class="navbar-brand">Api接口文档</a>
-        <a href="/doc/apiDebug" class="navbar-brand">Api接口调试</a>
-        <a href="/doc/dbStructDocuments" class="navbar-brand">数据库表结构</a>
-        <a href="/doc/apiDevDocuments" class="navbar-brand">接口开发说明文档</a>
-        <a href="/doc/apiPublicDocuments" class="navbar-brand">公共文档</a>
+        <a href="/doc/doc/documents" class="navbar-brand">Api接口文档</a>
+        <a href="/doc/doc/apiDebug" class="navbar-brand">Api接口调试</a>
+        <a href="/doc/doc/dbStructDocuments" class="navbar-brand">数据库表结构</a>
+        <a href="/doc/doc/apiDevDocuments" class="navbar-brand">接口开发说明文档</a>
+        <a href="/doc/doc/apiPublicDocuments" class="navbar-brand">公共文档</a>
       </div>
     </div>
     <nav id="bs-navbar" class="collapse navbar-collapse">
